00d7a0825fe65ee9157ee52738a98c34c482f53c
Xavier <xguenel@adivsa.fr>
2024-11-09 14:28:44 +0100

    Added MakeCrud command

--- Console/Command/MakegentoCrudCommand.php
+++ Console/Command/MakegentoCrudCommand.php
@@ -1,8 +1,51 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Opengento\MakegentoCli\Console\Command;
 
-class MakegentoCrudCommand
-{
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Magento\Framework\App\State;
+use Magento\Framework\App\Area;
 
+/**
+ * Copyright © OpenGento, All rights reserved.
+ * See LICENSE bundled with this library for license details.
+ */
+class MakegentoCrudCommand extends Command
+{
+    public function __construct(
+        private readonly State $appState,
+    ) {
+        parent::__construct();
+    }
+
+    /**
+     * Initialization of the command.
+     */
+    protected function configure()
+    {
+        $this->setName('makegento:crud')
+            ->setDescription('makegento let you do everything you want');
+        parent::configure();
+    }
+
+    /**
+     * CLI command description.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->appState->setAreaCode(Area::AREA_GLOBAL);
+
+        $output->writeln("<info>Vive Opengento</info>");
+        return Command::SUCCESS;
+    }
 }
